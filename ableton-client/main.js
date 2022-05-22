65cb638bd9c75aa5637d029ad28bc837b8affb0a
charlie-king4967 <charlie.king4967@gmail.com>
2022-05-22 12:40:29 -0700

    Add initial ableton client

---
+++ ableton-client/main.js
@@ -0,0 +1,15 @@
+const max = require("max-api");
+const WebSocket = require("ws");
+
+
+const URL = "ws://localhost:8000/ws"
+
+
+const ws = new WebSocket(URL);
+ws.on("error", () => {
+    max.post("error");
+})
+ws.on("message", data => {
+    note = JSON.parse(data);
+    max.outlet(note.pitch, note.velocity);
+})
