033404ce9459db94cef212976593a310ee0ea906
Charlie King <charlie.king@verkada.com>
2022-06-30 21:36:39 -0700

    Update web client

--- web-client/script.js
+++ web-client/script.js
@@ -1,21 +1,29 @@
-const URL = "ws://localhost:8000/publish";
+const connectButton = document.getElementById("connect_button");
+const noteButton = document.getElementById("note_button");
+const urlInput = document.getElementById("url");
+const idInput = document.getElementById("client_id");
 
 
-const button = document.getElementsByTagName("button")[0];
+connectButton.onclick = function() {
+    const host = urlInput.value;
+    const id = idInput.value;
+    const url = `${host}/publish/${id}`
 
-const ws = new WebSocket(URL);
+    const ws = new WebSocket(url);
 
-ws.onerror = function(error) {
-    console.log(error);
-};
+    ws.onerror = function(error) {
+        console.log(error);
+    };
 
-ws.onopen = function() {
-    console.log("Connected");
-    button.onclick = function() {
-        const note = {
-            pitch: 60,
-            velocity: 100,
+    ws.onopen = function() {
+        console.log("Connected");
+        connectButton.innerText = "Connected!";
+        noteButton.onclick = function() {
+            const note = {
+                pitch: 60,
+                velocity: 100,
+            };
+            ws.send(JSON.stringify(note));
         };
-        ws.send(JSON.stringify(note));
     };
 };
