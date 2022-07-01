99984beb8b8b81e4fb8a035858e907aeab8b8b89
Charlie King <charlie.king@verkada.com>
2022-06-30 20:58:15 -0700

    Publish notes via client_id rather than publishing to all

--- server/ableton_ws/main.py
+++ server/ableton_ws/main.py
@@ -1,8 +1,7 @@
-import asyncio
 import logging
-from typing import Set
+from uuid import UUID
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
 
 from .models import Note
 
@@ -13,47 +12,39 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
-class ConnectionManager:
-    def __init__(self):
-        self._websockets: Set[WebSocket] = set()
-
-    async def publish(self, note: Note):
-        cos = [
-            websocket.send_json(note.dict()) for websocket in self._websockets
-        ]
-        await asyncio.gather(*cos)
-
-    def subscribe(self, websocket: WebSocket):
-        self._websockets.add(websocket)
-
-    def unsubscribe(self, websocket: WebSocket):
-        self._websockets.remove(websocket)
+clients: dict[UUID, WebSocket] = {}
 
 
-manager = ConnectionManager()
-
-
-@app.websocket("/receive")
-async def get_notes(websocket: WebSocket):
+@app.websocket("/receive/{client_id}")
+async def get_notes(websocket: WebSocket, client_id: UUID):
     await websocket.accept()
     logger.info("Receive client connected")
-    manager.subscribe(websocket)
+    clients[client_id] = websocket
     try:
         while True:
             await websocket.receive_json()
     except WebSocketDisconnect:
         logger.info("Receive client disconnected")
-        manager.unsubscribe(websocket)
+        del clients[client_id]
 
 
-@app.websocket("/publish")
-async def publish_notes(websocket: WebSocket):
+@app.websocket("/publish/{client_id}")
+async def publish_notes(websocket: WebSocket, client_id: UUID):
+    if client_id not in clients:
+        raise HTTPException(status_code=404, detail="Client not found")
+
     await websocket.accept()
     logger.info("Publish client connected")
     try:
         while True:
             data = await websocket.receive_json()
             note = Note.parse_obj(data)
-            await manager.publish(note)
+            ws = clients.get(client_id)
+            # Check if client has disconnected
+            if ws is None:
+                await websocket.close(code=1001, reason="Client disconnected")
+        
+            await ws.send_json(note.dict())
+
     except WebSocketDisconnect:
         logger.info("Publish client disconnected")
